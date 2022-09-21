57e3e6f950d453eeb6017b0981276040be63d7cd
Hana <h.anakubo.sys22@morijyobi.ac.jp>
2022-09-21 12:35:43 +0900

    pra2.javaに出力処理追記
    Hello Worldを１００回出力するプログラムに変更

--- pra2.java
+++ pra2.java
@@ -1,4 +1,7 @@
-public static void main(String[] args) {
-	for(i=0,i<=100,i++){
-	System.out.println("Hello World):
+public class pra2{
+	public static void main(String[] args) {
+		for(i=0,i<=100,i++){
+		System.out.println("Hello World):
+		}
 	}
+}
